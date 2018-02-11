c35f8d38d09d556e1c8695d34da4a3e452598035
Juan Pablo Ascencio <jpabloascencioglez@gmail.com>
2018-02-10 18:07:00 -0600

    added the spider

---
+++ basicWeb/spider.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+def spider(max_pages):
+    page = 1
+    while page <= max_pages:
+        url = "http://www.9gag.com"
+        source_code = requests.get(url)
+        text_file = source_code.text
+        soup = BeautifulSoup(text_file, "lxml")
+        for link in soup.findAll("header"):
+            link = link.string
+            print(link)
+        page =+ 1
+spider(1)
